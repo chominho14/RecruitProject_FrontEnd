32acbac549a7a3c6b4c7b40c45424135fa47c836
chominho14 <chominho14@naver.com>
2023-04-30 17:52:18 +0900

    feat - Data Fetch Libs - Post 요청과 함께 authorization에 email 담아서 보내는 hook

---
+++ src/Libs/useMutationH.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+// 백엔드로 POST 타입으로 요청 시 json 형식으로 데이터를 주고 받는 라이브러리
+function useMuatationH(url) {
+  const [state, setState] = useState({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+
+  function mutation(data) {
+    const sessionEmail = localStorage.getItem("userData");
+    setState((prev) => ({ ...prev, loading: true }));
+
+    fetch("http://localhost:8080/api" + url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: sessionEmail,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json().catch(() => {}))
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
+  }
+  return [mutation, { ...state }];
+}
+
+export default useMuatationH;
